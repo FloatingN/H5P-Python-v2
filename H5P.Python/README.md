1748696d5224be38beb4e31a059288a4cdea4005
FloatingN <70947047+FloatingN@users.noreply.github.com>
2023-05-24 01:47:53 +0300

    Total cleanup

--- H5P.Python/README.md
+++
@@ -1,2 +0,0 @@
-# H5P Python
-This H5P content type allow you to create automatically corrected python exercises.
--- H5P.Python/src/entries/h5p-python.js
+++
@@ -1,6 +0,0 @@
-import "../styles/h5p-python.css";
-import Python from "../scripts/h5p-python";
-
-// Load library
-H5P = H5P || {};
-H5P.Python = Python;
